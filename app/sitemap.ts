03a8e1696161c11d08fe9e2c04296094ba6fb6e5
prohotelai <waleed94@hotmail.com>
2026-09-19 01:01:14 +0300

    feat: refresh corporate facts and discoverability

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://proinvest-global.com';
+  const baseUrl = 'https://proinvest.global';
   const lastModified = new Date();
   
   // Main pages (English - default)
