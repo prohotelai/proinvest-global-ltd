66d9e712871418dde2d4d9b784369fc91b1294d4
prohotelai <waleed94@hotmail.com>
2026-09-19 01:11:18 +0300

    seo: keep private partner and API surfaces out of crawling

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,18 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/ppn/', '/api/'],
+      },
+      {
+        userAgent: ['GPTBot', 'ChatGPT-User', 'Google-Extended', 'PerplexityBot'],
+        allow: '/',
+        disallow: ['/ppn/', '/api/'],
+      },
+    ],
     sitemap: 'https://proinvest.global/sitemap.xml',
   };
 }
